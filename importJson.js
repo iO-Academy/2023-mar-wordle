999460dadc0689f1a01b3b247b8eee828dea3224
Adam Young <adamyoungy678@gmail.com>
2023-04-18 12:09:07 +0100

    fetched words json then shuffled and output the first word

--- importJson.js
+++ importJson.js
@@ -2,7 +2,7 @@ function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
 
     // While there remain elements to shuffle.
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
 
         // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -15,3 +15,14 @@ function shuffle(array) {
 
     return array;
 }
+
+fetch('words.json')
+    .then(response => response.json())
+    .then(words => {
+        const wordsArr = words['fiveLetterWords']
+        const shuffledWords = shuffle(wordsArr); // Shuffle the array
+        const expectedWord = shuffledWords[0]; // Select the first word
+        console.log(`Expected Word: ${expectedWord}`);
+    })
+    .catch(error => console.error(`An error occurred: ${error.message}`));
+
