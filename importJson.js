9f8821b7b8d6650c59d371701c54ecce15e48880
Adam Young <adamyoungy678@gmail.com>
2023-04-18 13:58:30 +0100

    removed comments

--- importJson.js
+++ importJson.js
@@ -1,14 +1,11 @@
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
 
-    // While there remain elements to shuffle.
     while (currentIndex !== 0) {
 
-        // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
-        // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
             array[randomIndex], array[currentIndex]];
     }
@@ -20,9 +17,8 @@ fetch('words.json')
     .then(response => response.json())
     .then(words => {
         const wordsArr = words['fiveLetterWords']
-        const shuffledWords = shuffle(wordsArr) // Shuffle the array
-        const expectedWord = shuffledWords[0].toUpperCase().split("") // Select the first word
+        const shuffledWords = shuffle(wordsArr)
+        const expectedWord = shuffledWords[0].toUpperCase().split("")
         console.log(`Expected Word: ${expectedWord}`)
     })
     .catch(error => console.error(`An error occurred: ${error.message}`))
-
